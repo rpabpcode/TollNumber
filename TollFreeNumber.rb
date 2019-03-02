ccb2087986f6802cd6c6081477cd48e212b652cc
rpabp <rpabp.code@gmail.com>
2019-03-02 13:15:53 +0530

    Add dictionary.txt file values to hash of array

--- TollFreeNumber.rb
+++ TollFreeNumber.rb
@@ -2,7 +2,14 @@ require 'byebug'
 
 FILE_PATH = "dictionary.txt"
 
-def letter_combinations(numbers)
+def letter_combinations()
+  puts "Please Enter 10 digit Number - Except 0 & 1 digits : "
+  numbers = check_string(gets.chomp)
+  unless numbers # Allow only numbers
+    p "Plese enter only Numbers!"
+    return
+  end
+
   # verify the number is valid and does not contains eihter "0" or "1"
   if numbers.nil? || numbers.length != 10 || numbers.count("01") > 0
     p "Not a valid Number and Exiting!"
@@ -19,7 +26,16 @@ def letter_combinations(numbers)
   letters = {"2" => ["a", "b", "c"], "3" => ["d", "e", "f"], "4" => ["g", "h", "i"], "5" => ["j", "k", "l"],
              "6" => ["m", "n", "o"], "7" => ["p", "q", "r", "s"], "8" => ["t", "u", "v"], "9" => ["w", "x", "y", "z"]}
 
-
+  dictionary = {}
+  (1..20).each {|i| dictionary[i] = []}
+  #Read dictionary file and assign values to array
+  File.foreach(FILE_PATH) {|word| dictionary[word.length] << word.chop.to_s.downcase}
+  # map each number array
+  keys = numbers.chars.map {|number| letters[number]}
 end
 
-letter_combinations("4646525492")
+def check_string(string)
+  string.scan(/\D/).empty?
+end
+
+letter_combinations
